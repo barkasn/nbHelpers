b5ff4c4f4ab7cfafe42c763a58398a7b98b0da65
barkasn <Nikolas.barkas@outlook.com>
2017-12-17 14:31:46 -0500

    add return param ot preserve.state

--- R/main.R
+++ R/main.R
@@ -542,9 +542,11 @@ uniqCombs <- function(nms) {
 #' Save the session in the current working directory with a file name that
 #' includes the time stamp and process id
 #' @param prefix the prefix to use,for the filename
+#' @return the name of the file that was saved in
 #' @export preserve.state
 preserve.state <- function(prefix='savepoint_') {
   file <- paste0(prefix,gsub(' ','_',Sys.time()),'_',Sys.getpid(),'.RDataF')
   save.image.fast(file)
+  file
 }
 
