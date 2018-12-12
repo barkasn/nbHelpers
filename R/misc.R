1242f43c5f4c141a9831a2c578c1949a991e888d
Nikolas Barkas <nikolas.barkas@outlook.com>
2018-12-12 16:00:39 -0500

    Add transpose_dgRMatrix()

--- R/misc.R
+++ R/misc.R
@@ -361,3 +361,20 @@ sparseMatrixDensity <- function(mat) {
   dim0 <- dim(mat)
   sum(mat@x > 0) / (dim0[1] * dim0[2])
 }
+
+#' Transpose a dgRMatrix fast
+#' @param inmat input matrix in dgRMatrix format
+#' @return A dgCMatrix that is the transposed dgRMatrix
+#' @export transpose_dgRMatrix
+transpose_dgRMatrix <- function(inmat) {
+    if(class(inmat) != 'dgRMatrix')
+        stop('inmat is not of class dgRMatrix')
+    out <- new('dgCMatrix',
+               i=inmat@j,
+               p=inmat@p,
+               x=inmat@x,
+               Dim=rev(inmat@Dim),
+               Dimnames=rev(inmat@Dimnames)
+               )
+    out
+}                                 
