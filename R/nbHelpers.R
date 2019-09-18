545c1549b35140bafc16f37e166d7ce72db517ca
Nick Barkas <nikolas.barkas@outlook.com>
2019-09-18 16:24:53 -0400

    updates to imports

---
+++ R/nbHelpers.R
@@ -0,0 +1,11 @@
+#' nbHelpers
+#'
+#' A package with a collection of helper functions for bioinformatics analyses
+#'
+#' @docType package
+#' @author Nikolas Barkas
+#' @import Matrix utils stringdist matrixcalc gtools Rcpp
+#' @importFrom Rcpp evalCpp
+#' @useDynLib nbHelpers
+#' @name nbHelpers
+NULL
